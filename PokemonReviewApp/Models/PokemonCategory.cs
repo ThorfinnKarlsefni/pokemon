1311431d260600a3cf4b90e60a61f35d8300d81c
Cheung <cheung@Cheungs-Mini.lan>
2023-05-16 11:29:07 +0800

    seed

--- PokemonReviewApp/Models/PokemonCategory.cs
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -3,8 +3,8 @@ namespace PokemonReviewApp.Models
 {
     public class PokemonCategory
     {
-        public Pokemon PokemonId { get; set; }
-        public Category CategoryId { get; set; }
+        public int PokemonId { get; set; }
+        public int CategoryId { get; set; }
         public Pokemon Pokemon { get; set; }
         public Category Category { get; set; }
     }
--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -4,7 +4,7 @@ namespace PokemonReviewApp.Models
     public class Review
     {
         public int Id { get; set; }
-        public string? Titile { get; set; }
+        public string? Title { get; set; }
         public string? Text { get; set; }
         public int Rating { get; set; }
         public Reviewer Reviewer { get; set; }
