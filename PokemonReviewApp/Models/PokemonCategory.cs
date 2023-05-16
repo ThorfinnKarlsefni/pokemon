acfcc7e132a499d608ede6f038839e2805a768f8
Cheung <cheung@Cheungs-Mini.lan>
2023-05-16 09:08:24 +0800

    create model

---
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+    public class PokemonCategory
+    {
+        public Pokemon PokemonId { get; set; }
+        public Category CategoryId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
+
